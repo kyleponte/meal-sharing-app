79ac16e823f5c54f5a1513e6b6693c0a6b0e288c
Kyle <kyleponte07@gmail.com>
2025-04-02 23:02:10 -0400

    Revert "Merge branch 'main' of https://github.com/kyleponte/meal-sharing-app"
    This reverts commit 21f1559fbbd0dd8897e7dc2569fe507960a3eeb2, reversing
    changes made to 73f190d42bc0f68de1685cb0137edcd42dabd9af.

--- app.py
+++ app.py
@@ -31,35 +31,5 @@ class Meal:
 def home():
     return render_template('index.html')
 
-@app.route('/post-meal', methods=['GET', 'POST'])
-def post_meal():
-    if request.method == 'POST':
-        host_name = request.form['host']
-        location = request.form['location']
-        date_str = request.form['meal_date']
-        time_str = request.form['meal_time']
-        time = datetime.strptime(f"{date_str} {time_str}", "%Y-%m-%d %H:%M")
-        max_guests = int(request.form['max_guests'])
-        fee = float(request.form['fee'])
-        active_hours = int(request.form['active_hours'])
-
-        # Process dishes
-        dishes = []
-        for i in range(1, 4):  # Appetizer, Main, Dessert
-            dish_name = request.form[f'dish{i}_name']
-            ingredients = request.form[f'dish{i}_ingredients'].split(',')
-            course = ['appetizer', 'main', 'dessert'][i - 1]
-            dishes.append(Dish(dish_name, ingredients, course))
-
-        meal = Meal(host_name, dishes, location, time, max_guests, fee, active_hours)
-        meals.append(meal)
-        return redirect(url_for('home'))
-    return render_template('post_meal.html')
-
-@app.route('/browse-meals')
-def browse_meals():
-    return render_template('browse_meals.html', meals=meals)
-
-# --------- Run App ---------
 if __name__ == '__main__':
     app.run(debug=True)
