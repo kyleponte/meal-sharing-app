7440892082fd4c199902798fc6ad8e142503fbfe
Kyle <kyleponte07@gmail.com>
2025-04-02 22:26:56 -0400

    Created the landing page for the Meal Sharing Application

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # meal-sharing-app
 Meal sharing application
+
+Start the app with these command:
+pip install -r requirements.txt
+python app.py
---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+
+# --------- In-Memory Storage ---------
+meals = []
+
+# --------- Data Classes ---------
+class Dish:
+    def __init__(self, name, ingredients, course_type):
+        self.name = name
+        self.ingredients = ingredients
+        self.course_type = course_type
+
+class Meal:
+    def __init__(self, host_name, dishes, location, time, max_guests, fee, active_hours):
+        self.host_name = host_name
+        self.dishes = dishes
+        self.location = location
+        self.time = time
+        self.max_guests = max_guests
+        self.fee = fee
+        self.active_until = datetime.now() + timedelta(hours=active_hours)
+        self.guest_requests = []
+
+    def is_active(self):
+        return datetime.now() <= self.active_until
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
